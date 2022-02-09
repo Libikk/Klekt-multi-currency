42c5228a1570ae4852e19b753d27faf08ae31aba
Libikk <libikpl@gmail.com>
2022-02-09 19:55:24 +0000

    chore: init client

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className=' bg-slate-500'>
+    test
+      </div>
+    </div>
+  );
+}
+
+export default App;
